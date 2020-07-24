b1340bac88efc2710d5c9497ee6b71a8c3f5272c
Shrike <de_shrike@hotmail.com>
2020-07-24 13:55:15 +0100

    Added Bunny

---
+++ c/bunny.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char BRIGHTRED[] = "\033[31;1m";
+char RESET[] = "\033[0m";
+
+int main(int argc, char *argv[])
+{
+    // printf(BRIGHTRED);
+    printf("\n");
+    printf("(\\___/) \n");
+    printf("(='.'=) \n");
+    printf("(\")_(\") \n");
+    printf("\n");
+    printf(RESET);
+}
