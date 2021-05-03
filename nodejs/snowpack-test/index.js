25a68721841c63b3054b1553ff7e8d3f7d47fbdb
Shrike <de_shrike@hotmail.com>
2021-05-03 21:19:11 +0200

    Added Snowpack test

---
+++ nodejs/snowpack-test/index.js
@@ -0,0 +1,9 @@
+// import { snakeCase } from 'change-case';
+
+let myString = "...Somebody said fair warning, Lord will strike that poor boy down. ";
+myString += "Turned from hunted into hunter, went to hunt somebody down...";
+
+document.getElementById("theText").innerText = myString;
+
+console.log("index.js loaded");
+
