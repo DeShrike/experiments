50c603c509fd1fd9c9e368013ec09316290b10f5
Shrike <de_shrike@hotmail.com>
2021-07-13 20:56:51 +0200

    Neopixel Test

--- python/neo2.py
+++ python/neo2.py
@@ -1,17 +1,17 @@
 from rpi_ws281x import *
 
-LED_COUNT = 18 * 12
+LED_COUNT = 13 # 18 * 12
 LED_PIN = 18
 
-LED_FREQ_HZ = 800_000
+LED_FREQ_HZ = 800000
 LED_DMA = 10
 LED_BRIGHTNESS = 125  # 0 - 255
 LED_INVERT = False
 LED_CHANNEL = 0
 
-help(PixelStrip)
+#help(PixelStrip)
 
-quit()
+#quit()
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
@@ -21,3 +21,4 @@ for x in range(LED_COUNT):
 
 strip.show()
 
+aa = input()
